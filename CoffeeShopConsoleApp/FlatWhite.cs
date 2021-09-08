7093f411a5fd9875c66f65683a75b5ddc68d9eae
pc <pc>
2021-09-08 23:45:37 +0200

    task8

---
+++ CoffeeShopConsoleApp/FlatWhite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleApp
+{
+    class FlatWhite : Coffee
+    {
+        public FlatWhite(int discount) 
+            : base(discount)
+        {
+        }
+
+        public override string Strength()
+        {
+            return "Medium";
+        }
+    }
+}
